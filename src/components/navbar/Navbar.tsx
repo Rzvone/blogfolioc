b02847a04bf4e6f4aeb678b3d33741502c3323d4
Rzvone <bobonea.razvan.ctin@gmail.com>
2024-01-11 10:49:41 +0200

    first commit for navbar functions

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,22 +1,23 @@
 'use client'
 import Link from 'next/link'
 import React, { useState } from 'react'
+import AuthLinks from '../authLinks/AuthLinks'
+import ThemeToggle from '../themeToggle/themeToggle'
 
 
 
 const Navbar = () => {
-
-  const [nav, setNav] = useState(false)
-  const openNav = () => setNav(true)
   
   return (
-    <div className='container-nav'>
+    <div className='container-nav select-none'>
         <div className='logo'>raz<span className='text-green-600'>van</span></div>
         <div className='links'>
+          <ThemeToggle />
           <Link href='/'>Homepage</Link>
           <Link href='/about'>About</Link>
           <Link href='/contact'>Contact</Link>
           <Link href='/blog'>Blog</Link>
+          <AuthLinks />
         </div>
     </div>
   )
