558e16bafec526a74973632a8d4148a224fa5013
Rzvone <bobonea.razvan.ctin@gmail.com>
2024-01-23 16:20:38 +0200

    added nextrequest instad of url

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,35 +1,31 @@
 import prisma from '@/utils/connect';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export const GET = async (req: { url: string | URL }) => {
-
-    const { searchParams } = new URL(req.url)
+export const GET = async (req: NextRequest) => {
+    const { searchParams } = new URL(req.url);
 
     const page = Number(searchParams.get('page')) || 1;
     const cat = searchParams.get('cat') || null;
 
-
-    const POST_PER_PAGE = 4
+    const POST_PER_PAGE = 4;
 
     const query = {
-      take: POST_PER_PAGE,
-      skip: POST_PER_PAGE * (page - 1),
-      where: {
-        ...(cat && { catSlug: cat}),
-      }
-    }
-
+        take: POST_PER_PAGE,
+        skip: POST_PER_PAGE * (page - 1),
+        where: {
+            ...(cat && { catSlug: cat }),
+        },
+    };
 
     try {
-
         const [posts, count] = await prisma.$transaction([
-          prisma.post.findMany(query),
-          prisma.post.count()
-        ])
+            prisma.post.findMany(query),
+            prisma.post.count(),
+        ]);
 
-        return new NextResponse(JSON.stringify({posts, count,  status: 200}))
+        return new NextResponse(JSON.stringify({ posts, count, status: 200 }));
     } catch (error) {
-        console.log(error)
-        return new NextResponse(JSON.stringify({ message: 'Something went wrong', status: 500}))
+        console.error(error);
+        return new NextResponse(JSON.stringify({ message: 'Something went wrong', status: 500 }));
     }
-}
+};
