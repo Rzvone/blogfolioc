04409ced40367f0c673e0e1180590a549fa9f772
Rzvone <bobonea.razvan.ctin@gmail.com>
2024-01-23 14:37:05 +0200

    small mod to blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,17 +2,22 @@
 'use client'
 import CardList from '@/components/cardList/CardList'
 import Menu from '@/components/menu/Menu'
-import MenuCategories from '@/components/menuCategories/MenuCategories'
 import ParticleBlog from '@/components/particles/ParticleBlog'
 import React from 'react'
 
-const Blog = () => {
+const Blog = ({searchParams}) => {
+
+  const page = parseInt(searchParams.page) || 1;
+  const { cat } = searchParams;
+
+  console.log(cat)
+
   return (
     <div className='min-h-[88vh] mx-auto ml-20 mr-20 text-white mt-[8vh]'>
       <ParticleBlog />
-      <h1 className='z-[10] bg-pink-700 px-2 py-2.5 text-center rounded-lg'>Blogg categorie</h1>
+      <h1 className='z-[10] bg-pink-700 px-2 py-2.5 text-center rounded-lg'>{cat} categorie</h1>
       <div className='z-[10] flex gap-[50px] mt-10 mx-5'>
-          <CardList />
+          <CardList page={page} cat={cat} />
           <Menu />    
       </div>
     </div>
